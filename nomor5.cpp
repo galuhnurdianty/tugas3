bf689992d720b059a84aface564afe2bd0f5aa5f
galuhnurdianty <32261792+galuhnurdianty@users.noreply.github.com>
2017-11-07 21:29:55 -0800

    menampilkan hasil Normal

--- nomor5.cpp
+++ nomor5.cpp
@@ -14,4 +14,8 @@ int main(){
     
     if (weight<height/2.5)
     cout << "Result : Underweight" << endl;
+    
+    else if (height/2.5 <= weight <= height/2.3)
+    cout << "Result : Normal" << endl;
+    
 }
