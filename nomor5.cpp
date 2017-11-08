40b6fb4e052e90b79a1f09265fe5d1362577aad1
galuhnurdianty <32261792+galuhnurdianty@users.noreply.github.com>
2017-11-07 21:24:45 -0800

    memasukkan tinggi badan

--- nomor5.cpp
+++ nomor5.cpp
@@ -5,4 +5,8 @@ using namespace std;
 int main(){
 	float weight, underweight, normal, overweight;
     int height;
+    
+    cout << "Silahkan Masukkan Tinggi Badan Anda dalam cm : " << endl;
+    cin >> height;
+
 }
